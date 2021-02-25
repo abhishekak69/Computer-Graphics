b01943b9733c8f84306bfef8ac0cf1f3a622346c
Abhishek Kumar <abhishek.ak69@gmail.com>
2021-02-25 17:23:17 +0530

    refactored projection to work with vec3

--- projection.py
+++ projection.py
@@ -1,9 +1,8 @@
-import numpy as np
 import pygame
 import time
-from vector_class import vec3
+from vector_class import vec3, vec2
 
-O = (0, 0, 0)
+O = vec3()
 Cw = 800
 Ch = 800
 size = [Cw, Ch]
@@ -14,23 +13,21 @@ Vh = 1
 inf = 999
 projection_plane_d = 1
 # BACKGROUND_COLOR = (255, 255, 255)
-BACKGROUND_COLOR = (0, 0, 0)
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
+BACKGROUND_COLOR = vec3()
+BLUE = vec3(0, 0, 255)
+GREEN = vec3(0, 255, 0)
+RED = vec3(255, 0, 0)
 
 
 def putpixel(Cx, Cy, color):
     Sx = (Cw/2)+Cx
     Sy = (Ch/2)-Cy
-    pygame.draw.circle(screen, color, (Sx, Sy), 1)
+    pygame.draw.circle(screen, (color.x, color.y, color.z), (Sx, Sy), 1)
 
 
 def interpolate(i0, d0, i1, d1):
     if i0 == i1:
-        # print("equal", i0, d0, i1, d1)
         return [d0]
-    # print(i0, d0, i1, d1)
     values = []
     a = (d1 - d0) / (i1 - i0)
     d = d0
@@ -40,57 +37,57 @@ def interpolate(i0, d0, i1, d1):
     return values
 
 
-print(interpolate(0, 0, 0, -150))
+# print(interpolate(0, 0, 0, -150))
 
 
 def drawLine(p0, p1, color):
-    x0, y0, x1, y1 = p0[0], p0[1], p1[0], p1[1]
+    x0, y0, x1, y1 = p0.x, p0.y, p1.x, p1.y
     # print(x0, y0, x1, y1)
     if abs(x1 - x0) > abs(y1 - y0):
         if x0 > x1:
             p0, p1 = p1, p0
 
-        ys = interpolate(p0[0], p0[1], p1[0], p1[1])
-        for x in range(int(p0[0]), int(p1[0])+1):
-            putpixel(x, ys[int(x - p0[0])], color)
+        ys = interpolate(p0.x, p0.y, p1.x, p1.y)
+        for x in range(int(p0.x), int(p1.x)+1):
+            putpixel(x, ys[int(x - p0.x)], color)
 
     else:
         if y0 > y1:
             p0, p1 = p1, p0
-        xs = interpolate(p0[1], p0[0], p1[1], p1[0])
-        for y in range(int(p0[1]), int(p1[1])+1):
-            putpixel(xs[int(y - p0[1])], y, color)
+        xs = interpolate(p0.y, p0.x, p1.y, p1.x)
+        for y in range(int(p0.y), int(p1.y)+1):
+            putpixel(xs[int(y - p0.y)], y, color)
 
 
 def ViewportToCanvas(x, y):
-    return (int(x * Cw/Vw), int(y * Ch/Vh))
+    return vec2(int(x * Cw/Vw), int(y * Ch/Vh))
 
 
 def ProjectVertex(v):
-    return ViewportToCanvas(v[0] * d / v[2], v[1] * d / v[2])
+    return ViewportToCanvas(v.x * d / v.z, v.y * d / v.z)
 
 
-def drawTriangle(p0, p1, p2, color):
-    drawLine(p0, p1, color)
-    drawLine(p1, p2, color)
-    drawLine(p2, p0, color)
+# def drawTriangle(p0, p1, p2, color):
+#     drawLine(p0, p1, color)
+#     drawLine(p1, p2, color)
+#     drawLine(p2, p0, color)
 
 
 # The four "front" vertices
-vAf = [-1,  1, 5]
-vBf = [1,  1, 5]
-vCf = [1, -1, 5]
-vDf = [-1, -1, 5]
+vAf = vec3(-1,  1, 5)
+vBf = vec3(1,  1, 5)
+vCf = vec3(1, -1, 5)
+vDf = vec3(-1, -1, 5)
 
 # The four "back" vertices
-vAb = [-1,  1, 6]
-vBb = [1,  1, 6]
-vCb = [1, -1, 6]
-vDb = [-1, -1, 6]
+vAb = vec3(-1,  1, 6)
+vBb = vec3(1,  1, 6)
+vCb = vec3(1, -1, 6)
+vDb = vec3(-1, -1, 6)
 
-print(ProjectVertex(vAf), ProjectVertex(vBf), ProjectVertex(
-    vCf), ProjectVertex(vDf), ProjectVertex(vAb))
-print(ProjectVertex(vBb), ProjectVertex(vCb), ProjectVertex(vDb))
+# print(ProjectVertex(vAf), ProjectVertex(vBf), ProjectVertex(
+#     vCf), ProjectVertex(vDf), ProjectVertex(vAb))
+# print(ProjectVertex(vBb), ProjectVertex(vCb), ProjectVertex(vDb))
 
 
 pygame.init()
